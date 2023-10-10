2143b7dddcf29de0a90871b1b88bfc17b480667b
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-10 14:42:00 +0200

    dev: added http server with http library
    use of a low level library.
    So we should move to a better library...
    express

--- back/server.js
+++ back/server.js
@@ -1,4 +1,23 @@
 console.log("About to start a server...");
 
 const http = require("node:http");
-console.log("http: ", http);
+const fs = require("node:fs");
+
+const server = http.createServer((req, res) => {
+  const path = req.url;
+  console.log("path: ", path);
+  if (path === "/favicon.ico") {
+    res.writeHead(200, {
+      "Content-Type": "image/svg+xml",
+    });
+    const content = fs.readFileSync("./logo.svg", { encoding: "utf-8" });
+    res.end(content);
+    return;
+  }
+  res.writeHead(200, { "Content-Type": "text/plain", "X-Hello-World": "yyy" });
+  res.end("okay");
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
