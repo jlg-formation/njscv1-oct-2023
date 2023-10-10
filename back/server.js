be80ecc43ce71e78868a6c9b1083072c1e84f106
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-10 16:59:37 +0200

    dev: added transition middleware

--- back/server.js
+++ back/server.js
@@ -4,7 +4,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
+app.use((req, res, next) => {
+  console.log("req: ", req.method, req.url);
+  next();
+});
+
+app.use((req, res) => {
   res.send("Hello World!");
 });
 
