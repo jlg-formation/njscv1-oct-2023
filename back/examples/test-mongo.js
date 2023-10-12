96d6d0b447fc5c58fcbcf18e000a3bf00fa1c66f
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-12 11:48:08 +0200

    dev: added example client mongo

---
+++ back/examples/test-mongo.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require("mongodb");
+// Replace the uri string with your connection string.
+const uri = "mongodb://127.0.0.1:27017";
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    const database = client.db("gestion-stock");
+    const movies = database.collection("articles");
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { name: "Pelle" };
+    const article = await movies.findOne(query);
+    console.log(article);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
