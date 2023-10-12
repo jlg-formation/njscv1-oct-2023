32f2f5bfc0f92a269752b1a4bf362637ab4d364d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-12 09:08:51 +0200

    dev: const url

--- back/test/test.js
+++ back/test/test.js
@@ -2,35 +2,37 @@ const assert = require("assert");
 const axios = require("axios");
 const { a1 } = require("./data/articles");
 
+const url = "http://localhost:3000/api/articles";
+
 describe("Articles", function () {
   it("should get all articles", async () => {
-    let response = await axios.get("http://localhost:3000/api/articles");
+    let response = await axios.get(url);
 
     const ids = response.data.map((a) => a.id);
-    response = await axios.delete("http://localhost:3000/api/articles", {
+    response = await axios.delete(url, {
       data: ids,
     });
 
-    response = await axios.get("http://localhost:3000/api/articles");
+    response = await axios.get(url);
 
     assert.deepStrictEqual(response.data, []);
   });
 
   it("should add one article", async () => {
-    let response = await axios.post("http://localhost:3000/api/articles", a1);
+    let response = await axios.post(url, a1);
 
     assert.deepStrictEqual(response.status, 201);
     const keys = Object.keys(response.data);
     assert.deepStrictEqual(keys, ["id"]);
-    response = await axios.get("http://localhost:3000/api/articles");
+    response = await axios.get(url);
 
     assert.deepStrictEqual(response.data.length, 1);
   });
 
   it("get one article", async () => {
-    let response = await axios.get("http://localhost:3000/api/articles");
+    let response = await axios.get(url);
     const id = response.data[0].id;
-    response = await axios.get(`http://localhost:3000/api/articles/${id}`);
+    response = await axios.get(`${url}/${id}`);
 
     assert.deepStrictEqual(response.status, 200);
     const expectedA1 = { ...response.data };
