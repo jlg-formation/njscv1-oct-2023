5951b3d658d2482d8d2565b38bf0e62e298d4d7a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-12 16:49:42 +0200

    dev: added morgan for log

--- back/server.js
+++ back/server.js
@@ -1,6 +1,7 @@
 console.log("About to start a server...");
 import express from "express";
 import serveIndex from "serve-index";
+import morgan from "morgan";
 
 import api from "./api.js";
 
@@ -8,10 +9,7 @@ const app = express();
 const port = 3000;
 const publicDir = ".";
 
-app.use((req, res, next) => {
-  console.log("req: ", req.method, req.url);
-  next();
-});
+app.use(morgan("dev"));
 
 app.use(express.json());
 
