d98279856ab6ce72f0dcecff4ca55e7731c16258
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-10-13 12:14:26 +0200

    dev: refresh article

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,24 +1,15 @@
 import "./style.scss";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
-import { setupCounter } from "./counter.ts";
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+const url = "http://localhost:3000/api/articles";
 
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+const refreshArticles = async () => {
+  // ramener la liste d'articles du back
+  // trouver l'endroit ou afficher les articles
+  // les afficher
+
+  const response = await fetch(url);
+  const articles = await response.json();
+  console.log("articles: ", articles);
+};
+
+refreshArticles();
